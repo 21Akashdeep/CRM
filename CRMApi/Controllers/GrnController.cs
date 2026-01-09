dedd8f6d4fe779047fbd9c4a0dc43d4f1ac16c5f
Akashdeep <rahisinha21@gmail.com>
2026-01-09 14:13:40 +0530

    Grn Added

---
+++ CRMApi/Controllers/GrnController.cs
@@ -0,0 +1,106 @@
+﻿using CRMApi.Models;
+using CRMApi.Repository;
+using CRMApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class GrnController : Controller
+    {
+        private readonly DBCRM db;
+        private readonly RepoGrn RepoGrn;
+        public GrnController(DBCRM _db)
+        {
+            db = _db;
+            RepoGrn = new RepoGrn(db);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetViewOption()
+        {
+            Message objMsg = new Message();
+            try
+            {
+                var User = Util.RequestVerify(new Request { HttpRequest = Request, ActionType = ActionType.View }, db, ref objMsg);
+                if (User == null) return Ok(objMsg);
+                objMsg = await RepoGrn.GetViewOptionAsync();
+            }
+            catch (Exception ex)
+            {
+                Message.Exception(ref objMsg, ex);
+            }
+            return Ok(objMsg);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Get(Voucher obj)
+        {
+            Message objMsg = new Message();
+            try
+            {
+                var User = Util.RequestVerify(new Request { HttpRequest = Request, ActionType = ActionType.View }, db, ref objMsg);
+                if (User == null) return Ok(objMsg);
+                objMsg.data = await RepoGrn.ListAsync(obj, User);
+                Message.Get(ref objMsg, "");
+            }
+            catch (Exception ex)
+            {
+                Message.Exception(ref objMsg, ex);
+            }
+            return Ok(objMsg);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Location obj)
+        {
+            Message objMsg = new Message();
+            try
+            {
+                var User = Util.RequestVerify(new Request { HttpRequest = Request, ActionType = ActionType.Export }, db, ref objMsg);
+                if (User == null) return Ok(objMsg);
+                //objMsg = RepoGdn.Add(obj, User);
+            }
+            catch (Exception ex)
+            {
+                Message.Exception(ref objMsg, ex);
+            }
+            return Ok(objMsg);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Print(Voucher obj)
+        {
+            Message objMsg = new Message();
+            try
+            {
+                var User = Util.RequestVerify(new Request { HttpRequest = Request, ActionType = ActionType.Print }, db, ref objMsg);
+                if (User == null) return Ok(objMsg);
+                objMsg = await RepoGrn.PrintAsync(obj, User);
+            }
+            catch (Exception ex)
+            {
+                Message.Exception(ref objMsg, ex);
+            }
+            return Ok(objMsg);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Export(Voucher obj)
+        {
+            Message objMsg = new Message();
+            try
+            {
+                var User = Util.RequestVerify(new Request { HttpRequest = Request, ActionType = ActionType.Export }, db, ref objMsg);
+                if (User == null) return Ok(objMsg);
+                objMsg = await RepoGrn.ExportAsync(obj, User);
+            }
+            catch (Exception ex)
+            {
+                Message.Exception(ref objMsg, ex);
+            }
+            return Ok(objMsg);
+        }
+    }
+}
---
+++ CRMApi/Models/Store.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRMApi.Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        [MaxLength(16)] public string Code { get; set; } = String.Empty;
+        [MaxLength(100)] public string Name { get; set; } = String.Empty;
+        [MaxLength(200)] public string Description { get; set; } = String.Empty;
+        
+        public int Status { get; set; } = 1;
+        [NotMapped] public string StatusName { get; set; } = string.Empty;
+        [NotMapped] public string StatusCss { get; set; } = string.Empty;
+        [NotMapped] public string StatusIcon { get; set; } = string.Empty;
+        public int CreatedBy { get; set; }
+        [NotMapped] public string CreatedByName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int UpdatedBy { get; set; }
+        [NotMapped] public string UpdatedByName { get; set; } = string.Empty;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        [NotMapped] public bool IsEdit { get; set; } = false;
+        [NotMapped] public bool IsDelete { get; set; } = false;
+        [NotMapped] public bool IsEnable { get; set; } = false;
+        [NotMapped] public bool IsDuplicate { get; set; } = false;
+        [NotMapped] public List<int> ListId { get; set; } = new List<int>();
+        [NotMapped] public List<int> ListStatus { get; set; } = new List<int>();
+    }
+}
