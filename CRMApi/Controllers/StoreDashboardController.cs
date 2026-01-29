773e53335bd48e95f89e6e37ff05c690986bfc07
Sintu9155 <sintu9155624748@gmail.com>
2026-01-29 11:48:36 +0530

    Merge

---
+++ CRMApi/Controllers/StoreDashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRMApi.Controllers
+{
+    public class StoreDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
